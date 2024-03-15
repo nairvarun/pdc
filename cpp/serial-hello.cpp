0de1c34cf2cb7b8e5f54e847a50d537ea0766ddf
varun <74466129+nairvarun@users.noreply.github.com>
2024-03-15 11:52:48 +0530

    .

---
+++ cpp/serial-hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    const int numMessages = 5;
+    
+    for (int i = 0; i < numMessages; ++i) {
+        std::cout << "Hello, World!" << std::endl;
+    }
+
+    return 0;
+}
+
